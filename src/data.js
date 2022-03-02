b400b06e664ff140cce051a4d2960b1472173e5c
kn8a <mydreamhomefl@gmail.com>
2022-03-02 04:53:18 -0500

    workiing task cards

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const taskArray = [
+    {'id': '1', 'project': 'first project', 'title': `task1`, 'text': `this is some task that needs to be completed`, 'date': `28/02/2022`, 'priority': 'high'},
+    {'id': '2', 'project': 'first project', 'title': `task2`, 'text': `this is some task that needs to be completed`, 'date': `30/02/2022`, 'priority': 'high'},
+    {'id': '3', 'project': 'first project', 'title': `task2`, 'text': `this is some task that needs to be completed`, 'date': `30/02/2022`, 'priority': 'low'},
+    {'id': '2', 'project': 'not project', 'title': `task2`, 'text': `this is some task that needs to be completed`, 'date': `30/02/2022`, 'priority': 'high'},
+];
