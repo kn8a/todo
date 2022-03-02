b0cd7503e403e4b3c67267109ee0fd54af033d1e
kn8a <mydreamhomefl@gmail.com>
2022-03-02 07:40:29 -0500

    cards dynamically generated, UI improvements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const log = console.log;
 import { makeCards } from './tasksRender.js';
 import { taskArray } from './data.js'
 import './style.css';
+import './card.css';
+import './form.css';
 
 log(taskArray);
 
@@ -11,3 +13,23 @@ makeCards(taskArray, 'first project');
 
 
 
+const showForm = document.getElementById('showForm'); //new book button to variable
+showForm.onclick = function() { 
+  document.getElementById('popupForm').style.display = "block"; //show form
+  showForm.style.backgroundColor="transparent";//hide + button
+  showForm.style.color="transparent";
+  showForm.style.cursor="default" 
+}
+
+
+
+
+
+
+//function closeForm() {
+//  document.getElementById("popupForm").style.display = "none"; //close form
+//  document.getElementById("entryForm").reset();  //reset form fields
+//  showForm.style.backgroundColor="#EB5E28"; //restor + button
+//  showForm.style.color="#FFFCF2";
+//  showForm.style.cursor="pointer";
+//}
--- src/tasksRender.js
+++ src/tasksRender.js
@@ -1,56 +1,60 @@
+//takes tasks array and project name
+
 export function makeCards (task, project) {
-    const mainDiv = document.getElementById('main')  
-    
     for (let i=0; i < task.length; i++) {
         if (task[i].project == project) {
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('card');
-            cardDiv.id = task[i].id;
-            mainDiv.appendChild(cardDiv);
-            //title
-            const cardTitle = document.createElement('h4');
-            cardTitle.textContent = task[i].title;
-            cardDiv.appendChild(cardTitle);
-            //body cont
-            const cardText = document.createElement('p');
-            cardText.textContent = task[i].text;
-            cardDiv.appendChild(cardText);
-            //due date
-            const cardDate = document.createElement('p');
-            cardDate.textContent = task[i].date;
-            cardDiv.appendChild(cardDate);
-            //card bar
-            const cardBarDiv = document.createElement('div');
-            cardBarDiv.classList.add('card-bar');
-            cardDiv.appendChild(cardBarDiv);
-            //priority
-            const cardPriority = document.createElement('div')
-            cardPriority.classList.add(`priority-${task[i].priority}`);
-            cardPriority.textContent = `${task[i].priority} priority`;
-            cardBarDiv.appendChild(cardPriority);   
-            //task actions
-            const taskActions = document.createElement('div');
-            taskActions.classList.add('task-actions');
-            cardBarDiv.appendChild(taskActions);
-                //complete
-                const taskDone = document.createElement('div');
-                taskDone.classList.add('task-action');
-                taskDone.setAttribute('data-done', task[i].id);
-                taskDone.textContent = '✔️';
-                taskActions.appendChild(taskDone);
-
-                //edit
-                const taskEdit = document.createElement('div');
-                taskEdit.classList.add('task-action');
-                taskEdit.setAttribute('data-edit', task[i].id);
-                taskEdit.textContent = '🖊️';
-                taskActions.appendChild(taskEdit);
-                //delete
-                const taskDel = document.createElement('div');
-                taskDel.classList.add('task-action');
-                taskDel.setAttribute('data-del', task[i].id);
-                taskDel.textContent = '❌';
-                taskActions.appendChild(taskDel);
+            renderCard(task[i]);
         }
     }
+}
+
+function renderCard(task) {
+    const mainDiv = document.getElementById('main')
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    cardDiv.id = task.id;
+    mainDiv.appendChild(cardDiv);
+            //title
+    const cardTitle = document.createElement('h4');
+    cardTitle.textContent = task.title;
+    cardDiv.appendChild(cardTitle);
+            //body cont
+    const cardText = document.createElement('p');
+    cardText.textContent = task.text;
+    cardDiv.appendChild(cardText);
+            //due date
+    const cardDate = document.createElement('p');
+    cardDate.textContent = `Due date: ${task.date}`;
+    cardDiv.appendChild(cardDate);
+            //card bar
+    const cardBarDiv = document.createElement('div');
+    cardBarDiv.classList.add('card-bar');
+    cardDiv.appendChild(cardBarDiv);
+            //priority
+    const cardPriority = document.createElement('div')
+    cardPriority.classList.add(`priority-${task.priority}`);
+    cardPriority.textContent = `${task.priority} priority`;
+    cardBarDiv.appendChild(cardPriority);   
+            //task actions
+    const taskActions = document.createElement('div');
+    taskActions.classList.add('task-actions');
+    cardBarDiv.appendChild(taskActions);
+                //complete
+        const taskDone = document.createElement('div');
+        taskDone.classList.add('task-action');
+        taskDone.setAttribute('data-done', task.id);
+        taskDone.textContent = '✔️';
+        taskActions.appendChild(taskDone);
+                //edit
+        const taskEdit = document.createElement('div');
+        taskEdit.classList.add('task-action');
+        taskEdit.setAttribute('data-edit', task.id);
+        taskEdit.textContent = '🖊️';
+        taskActions.appendChild(taskEdit);
+                //delete
+        const taskDel = document.createElement('div');
+        taskDel.classList.add('task-action');
+        taskDel.setAttribute('data-del', task.id);
+        taskDel.textContent = '❌';
+        taskActions.appendChild(taskDel);
 }
